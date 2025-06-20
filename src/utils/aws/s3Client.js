997a9049d7ee0c9144bfad1e457af5dc1a2f55d3
sravanthi_12 <sravanthic133@gmail.com>
2025-06-20 11:33:09 +0530

    pulled updated code

--- src/utils/aws/s3Client.js
+++ src/utils/aws/s3Client.js
@@ -1,15 +1,17 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+
+
+dotenv.config();
 
 const REGION = "us-east-1";
-const ACCESS_KEY_ID = "AKIA44Y6CHJMLMOWKNCL";
-const SECRET_ACCESS_KEY = "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"
+
 const s3Client = new S3Client({
   region: REGION,
   credentials: {
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
   },
-  
 });
- 
+
 export { s3Client };
