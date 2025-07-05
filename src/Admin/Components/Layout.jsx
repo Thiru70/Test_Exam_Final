1f269a6d6815e2e8e3b42b2d5543aa9491671b39
Vishak-P <147609054+Vishak-P@users.noreply.github.com>
2025-07-05 17:43:45 +0530

    Update Layout.jsx

--- src/Admin/Components/Layout.jsx
+++ src/Admin/Components/Layout.jsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Components/Sidebar";
 import Navbar from "../Components/Navbar";
+import { useState } from "react";
 
 const Layout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div className="flex h-screen overflow-hidden">
-      <Sidebar />
+      <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
       <div className="flex-1 flex flex-col overflow-auto">
-        <Navbar />
+        <Navbar toggleSidebar={toggleSidebar} />
         <main className="flex-1 overflow-auto p-4">
           <Outlet />
         </main>
