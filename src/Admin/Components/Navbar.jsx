a1ed87571765fbee9d4f3d4040d3a60a26055c40
Thiru70 <147789617+Thiru70@users.noreply.github.com>
2025-07-07 20:29:51 +0530

    Update Navbar.jsx

--- src/Admin/Components/Navbar.jsx
+++ src/Admin/Components/Navbar.jsx
@@ -40,22 +40,25 @@ const Navbar = ({ toggleSidebar }) => {
     fetchProfileData();
   }, [email]);
   return (
-    <header onClick={()=>setOpenProfile(!openProfile)} className="flex items-center justify-end px-6 py-4 bg-white border-b sticky top-0 z-10">
-      <h1 className="bg-black text-white font-bold rounded-full w-10 h-10 flex justify-center items-center text-xl">
-        {profileIcon || "U"}
-      </h1>
+    <header onClick={()=>setOpenProfile(!openProfile)} className="absolute sticky top-0 flex items-center justify-end px-6 py-2 border-b">
+     <h1 className="flex items-center justify-center w-10 h-10 text-xl font-bold text-white bg-black border border-red-500 rounded-full">
+    <span className="flex items-center justify-center w-full h-full z-80 ">{profileIcon || "U"}</span>
+ 
+</h1>
+
+
       {
-        openProfile ? <div className="w-48 absolute top-14 bg-white z-40 shadow-2xl flex flex-col gap-2 p-2">
+        openProfile ? <div className="absolute z-40 flex flex-col w-48 gap-2 p-2 bg-white shadow-2xl top-16">
           <profileIcon/>
         
-       <Link classNam="flex items-center" ref={dropdownRef }  to="/account" className="px-2 hover:bg-zinc-200"
+       <Link classNam="flex items-center" ref={dropdownRef}  to="/account" className="px-2 hover:bg-zinc-200"
        >
         <div className="flex items-center gap-2">
         <Icon icon="mdi:account-circle-outline" width={24}/>
-        <span> My Account</span>
+          <span> My Account</span>
         </div>
        </Link>
-       <Link ref={dropdownRef }  onClick={handleLogout} to="/login" className="px-2 hover:bg-zinc-200">   <span className="flex gap-2"><LogOut className="h-6 w-6"/> Logout</span></Link>
+       <Link ref={dropdownRef }  onClick={handleLogout} to="/login" className="px-2 hover:bg-zinc-200">   <span className="flex gap-2"><LogOut className="w-6 h-6"/> Logout</span></Link>
         </div> : null
       }
 
